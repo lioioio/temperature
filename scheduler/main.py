8b82eb422dd081c51e5e7255cdae277cd04ddf89
lambda0 <lambda0@yandex.ru>
2020-03-15 15:04:00 +0300

    add scheduler

---
+++ scheduler/main.py
@@ -0,0 +1,128 @@
+import cmd
+import storage
+import json
+from pprint import pprint
+
+days = {'mon': 1 , 'tue': 2, 'etc...': 'etc...'}
+
+
+error_message = "Ошибка задания аргументов" #сообщение об ошибке, могут быть разными
+
+
+def is_day(args):
+    return args in "1234567" and int(args) < 8 and int(args) > 0
+
+class Cli(cmd.Cmd):
+
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = '> '
+        self.intro = "Приветствие" 
+        self.doc_header = "Доступные команды 'help _команда_'"
+
+    def do_get_schedule(self, args):
+        """Метод выводит расписание на неделю
+            или на определенный день недели 
+        """
+        if args == '':
+            # Если аргументы не заданы 
+            print(storage.get_schedule())
+        elif is_day(args):
+            # Если задан день недели
+            print(storage.get_schedule(args))
+        else:
+            print(error_message)
+
+    def do_change_schedule(self, args):
+        """
+            Метод задает расписание  на определенный
+            1 {"1": "Математика"}
+        """
+        args = args.split()
+        if len(args) > 1:
+            day = args[0]
+            schedule = ''.join(args[1:])
+            if is_day(day):
+                print(schedule)
+                try:
+                    storage.change_schedule(day, json.loads(schedule))
+                except Exception:
+                    print(error_message)
+            else:
+                print(error_message)
+        else:
+            print(error_message)
+
+    def do_add_note(self, args):
+        """Описание"""
+        args = args.split()
+        if len(args) > 2:
+            day = args[0]
+
+            if is_day(day):
+                event = args[1]
+                note = args[2:]
+                storage.add_note(day, event, note)
+                print("Заметка добавлена")
+            else:
+                print(error_message)
+        else:
+            print(error_message)
+
+    def do_add_todo(self, args):
+        """Описание"""
+        if len(args.split()) > 2:
+            day = args.split()[0]
+            event = args.split()[1]
+            note = args.split()[2:]
+
+            if is_day(day):
+                storage.add_note(day, event, note, todo=True)
+            else:
+                print(error_message)
+        else:
+            print(error_message)
+
+    def do_mark_as_done(self, args):
+        if len(args.split()) == 3:
+            day = args.split()[0]
+            event = args.split()[1]
+            note_num = args.split()[2]
+            storage.done(days.get(day), int(event), int(note_num))
+        else:
+            print(error_message)
+
+    def do_get_note(self, args):
+        """Выводит заметки"""
+        if len(args.split()) == 3:
+            day = args.split()[0]
+            event = args.split()[1]
+            note_num = args.split()[2]
+            if days.get(day) and event.isdigit() and note_num.isdigit():
+                storage.get_note(days.get(day), int(event), int(note_num))
+            else:
+                print(error_message)
+        else:
+            print(error_message)
+
+    def do_exit(self, args):
+        """Выход"""
+        exit()
+
+    def do_q(self, args):
+        """Выход"""
+        exit()
+
+    def default(self, line):
+        print(error_message)
+
+    def do_show_storage(self, args):
+        """Показывает содержимое хранилища"""
+        pprint(storage.storage)
+
+if __name__ == "__main__":
+    cli = Cli()
+    try:
+        cli.cmdloop()
+    except KeyboardInterrupt:
+        exit()
---
+++ scheduler/storage.py
@@ -0,0 +1,129 @@
+#! /usr/bin/env python3.7
+
+import json
+import os
+
+# пути
+PATH = {"file": "./storage"}
+
+# хранилище
+storage = {}
+
+
+def __init_storage():
+    """Функция инициализации хранилища."""
+    if not os.path.isfile(PATH["file"]):
+        # если файл хранилища данных нет
+        schedule = {}
+        for i in range(1, 8):
+            schedule[i] = {}
+        storage.update({"schedule": schedule, "note": {}})
+        # запись файла на диск
+        __dump()
+    else:
+        # загрузка файла
+        __load()
+
+
+def __load():
+    """Функция загрузки данных из файла."""
+    with open(PATH["file"]) as file:
+        storage.update(json.load(file))
+
+
+def __dump():
+    """Функция записывает данные в файл."""
+    with open(PATH["file"], "w") as file:
+        # ensure_ascii - для того чтобы кириллица не кодировалась в ascii
+        json.dump(storage, file, ensure_ascii=False)
+
+
+def crypt_db():
+    pass
+
+
+def decrypt_db():
+    pass
+
+
+def get_schedule(day=None):
+    """на всю неделю, при day != None на день, day in range(1, 8)"""
+    if day is None:
+        return storage["schedule"]
+    else:
+        return storage["schedule"].get(str(day), {})
+
+
+def change_schedule(day, schedule):
+    """Изменение расписания на день day in range(1, 8), type(schedule) == dict"""
+    storage["schedule"][str(day)] = schedule
+    __dump()
+
+
+def add_note(day, event, text, *, todo=None):
+    """Добавление "заметки" к "событию"
+        Если задан аргумент is_done, заметка становится "задачей".
+    """
+
+    # если в хранилище нет заметки с ключом (день,событие)
+    if storage["note"].get(f"({day},{event})") is None:
+        storage["note"][f"({day},{event})"] = {}
+
+    # индекс события (задачи)
+    id = len(storage["note"][f"({day},{event})"].keys()) + 1
+
+    storage["note"][f"({day},{event})"][id] = {
+        "id": id, "content": text, "is_done": None if todo is None else False
+    }
+    __dump()
+
+
+def get_notes(day, event):
+    """Функция возвращает заметки (задачи) для конкретного дня и события"""
+    return storage["note"][f"({day},{event})"]
+
+
+def get_all_notes():
+    return storage["note"]
+
+
+def get_notes_by_key(key):
+    """Функция возвращает заметки (задачи) по ключ: "день" или "событие"
+        Если нужно использовать "день" и "событие":
+            key = f"{day},{event}"
+    """
+    notes = {}
+    for dkey in storage["note"].keys():
+        if key in dkey:
+            notes[dkey] = storage["note"][dkey]
+    return notes
+
+
+def done(day, event, id):
+    """Функция помечает задачу выполненной """
+    if storage["note"].get(f"({day},{event})") is not None:
+        # если существует заметки для (день, событие)
+        if storage["note"][f"({day},{event})"].get(f"{id}") is not None:
+            # если существует заметка с id
+            note = storage["note"][f"({day},{event})"][f"{id}"]
+            if note["is_done"] is not None:
+                note["is_done"] = True
+                __dump()
+
+
+# это дополнительная функция
+# Для удаления "старых" заметок (задач)
+def delete_note(day, event, id):
+    """Удаление "заметки" ("задачи") из """
+    if storage["note"].get(f"({day},{event})") is not None:
+        # если существует заметки для (день, событие)
+        if storage["note"][f"({day},{event})"].get(str(id)) is not None:
+            # если существует заметка с id
+            storage["note"][f"({day},{event})"].pop(str(id))
+            if len(storage["note"][f"({day},{event})"].keys()) == 0:
+                storage["note"].pop(f"({day},{event})")
+            __dump()
+
+
+#  инициализация хранилища
+__init_storage()
